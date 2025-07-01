77dd03bb422909f3e307fed167b1974bc1c766db
Konrad <wi24b078@technikum-wien.at>
2025-07-01 13:19:53 +0200

    start new game method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("TicTacToe");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            TicTacToe game = new TicTacToe();
+            game.start();
+
+            System.out.print("Neues Spiel starten? (j/n): ");
+            String antwort = scanner.nextLine().trim().toLowerCase();
+
+            if (!antwort.equals("j")) {
+                System.out.println("Auf Wiedersehen!");
+                break;
+            }
+        }
     }
 }
